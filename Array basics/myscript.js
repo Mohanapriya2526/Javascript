f2a254abaea9ca6c362eb9cfdfe7369f332d5097
priya <priyadhanush37@gmail.com>
2023-07-12 11:46:55 +0530

    file replaced

---
+++ Array basics/myscript.js
@@ -0,0 +1,15 @@
+
+let numbers = [ ]
+let even = []
+let odd = []
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 == 0)
+
+        even.push(numbers[i])
+
+    else
+        odd.push(numbers[i])
+
+}
+console.log(even)
+console.log(odd)
