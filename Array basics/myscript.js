473d4fe5d39295a046a217699321fe0331796704
priya <priyadhanush37@gmail.com>
2023-07-12 12:44:22 +0530

    changed

--- Array basics/myscript.js
+++ Array basics/myscript.js
@@ -1,15 +1,23 @@
 
-let numbers = [ ]
+let number1 = [1, 2, 3, 4, 5 ]
+let number2 =[ 6, 7, 8, 9, 10]
+let num3= number1.concat(number2)
 let even = []
 let odd = []
-for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 == 0)
-
-        even.push(numbers[i])
-
-    else
-        odd.push(numbers[i])
-
+let sum = 0
+let sum1 = 0
+document.getElementById("demo").innerHTML = num3
+for (let i = 0; i < num3.length; i++) {
+    if (num3[i] % 2 == 0){
+       even.push(num3[i])
+        sum = sum +num3[i]
+    }
+    else {
+        odd.push(num3[i])
+        sum1 = sum1 +num3[i]}
 }
+console.log(num3)
 console.log(even)
+console.log(sum)
 console.log(odd)
+console.log(sum1)
