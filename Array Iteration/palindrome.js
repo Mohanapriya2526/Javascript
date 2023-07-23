4b1a19d0831494f5854b885e2ba52320b0ce3c68
priya <priyadhanush37@gmail.com>
2023-07-23 23:48:58 +0530

    palindrome

---
+++ Array Iteration/palindrome.js
@@ -0,0 +1,9 @@
+const str='malayalam';
+checkPalindrome(str);
+function checkPalindrome(str) {
+    const x = str.split('');
+    const y = x.reverse();
+    const z = y.join('');
+(str===z)?console.log("true"):console.log("false");
+  }
+  
