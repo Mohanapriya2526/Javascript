7789c7ebd2e7a3058678f1f3497fcae20d4aea77
priya <priyadhanush37@gmail.com>
2023-07-23 23:56:40 +0530

    vowels

---
+++ Array Iteration/vowels.js
@@ -0,0 +1,17 @@
+const name ="Mohanapriya S";
+const numberOfVowels =vowels(inputString);
+str = str.toLowercase();
+const vowels = ["a","e","i","o","u"]
+let count = 0;
+
+function vowels(str){
+    for (let i=0; i < vowels.length; i++ )
+    {
+        if (vowels.includes(str(i))){
+            count++;
+        }
+      }
+        return count;
+    }
+  console.log(inputString)
+    
