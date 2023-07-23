4e3aa818e88fecbae352a5098f545cabb9121a38
priya <priyadhanush37@gmail.com>
2023-07-23 23:53:31 +0530

    ques added

--- Array Iteration/ques5.js
+++ Array Iteration/ques5.js
@@ -1,4 +1,4 @@
-
+//5)Create an new array, it need to have element from numbers array multiple of three
     let number = [35, 14, 70, 49, 21, 56, 7, 42, 28, 63, 49];
     let num = number.map((num)=>num*3);
     console.log(num);
